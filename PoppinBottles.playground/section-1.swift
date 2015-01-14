f301d31c8ed626d2fa75bee1cb765fa931fb3a0a
Mike Reining <emailmike@gmail.com>
2015-01-13 23:14:04 -0800

    Task 2: Created Function that user amount and passes it to the model

--- PoppinBottles.playground/section-1.swift
+++ PoppinBottles.playground/section-1.swift
@@ -4,41 +4,50 @@ import Cocoa
 
 // Task 1 Build Model
 
-var customerInvestment = 20
-let pricePerBottle = 2
-var totalBottlesRecycled = 0
-var round = 1
+func poppinBottles (#enterInvestment: Int) {
+    println("INITIAL INVESTMENT: \(enterInvestment)")
+    println("")
 
-let initialBottles = customerInvestment / pricePerBottle
-let initialCaps = initialBottles
-
-var bottleSupply = initialBottles
-var capsSupply = initialCaps
-
-while bottleSupply > 1 || capsSupply > 3 {
-    println("Round: \(round)")
-    println("Bottle Supply: \(bottleSupply)")
-    var bottlesRecycled = bottleSupply - (bottleSupply % 2)
-    println("Bottles Redeemed: \(bottlesRecycled)")
-    totalBottlesRecycled += bottlesRecycled
-    bottleSupply -= bottlesRecycled
-    println("Caps Supply: \(capsSupply)")
-    var capsRecycled = capsSupply - (capsSupply % 4)
-    println("Caps Redeemed: \(capsRecycled)")
-    capsSupply -= capsRecycled
-
-    var newBottles = (bottlesRecycled / 2) + (capsRecycled / 4)
-    println("New Bottles: \(newBottles)")
-    bottleSupply += newBottles
-    capsSupply += newBottles
-    round += 1
-    println("We now have Bottles: \(bottleSupply) Caps: \(capsSupply)")
+    let pricePerBottle = 2
+    var totalBottlesRecycled = 0
+    var round = 1
+    
+    let initialBottles = enterInvestment / pricePerBottle
+    let initialCaps = initialBottles
+    
+    var bottleSupply = initialBottles
+    var capsSupply = initialCaps
+    
+    while bottleSupply > 1 || capsSupply > 3 {
+        println("Round: \(round)")
+        println("Bottle Supply: \(bottleSupply)")
+        var bottlesRecycled = bottleSupply - (bottleSupply % 2)
+        println("Bottles Redeemed: \(bottlesRecycled)")
+        totalBottlesRecycled += bottlesRecycled
+        bottleSupply -= bottlesRecycled
+        println("Caps Supply: \(capsSupply)")
+        var capsRecycled = capsSupply - (capsSupply % 4)
+        println("Caps Redeemed: \(capsRecycled)")
+        capsSupply -= capsRecycled
+        
+        var newBottles = (bottlesRecycled / 2) + (capsRecycled / 4)
+        println("New Bottles: \(newBottles)")
+        bottleSupply += newBottles
+        capsSupply += newBottles
+        round += 1
+        println("We now have Bottles: \(bottleSupply) Caps: \(capsSupply)")
+        println("")
+    }
+    
+    totalBottlesRecycled
+    round = 1
+    println("")
     println("")
 }
 
-totalBottlesRecycled
+poppinBottles(enterInvestment: 10)
 
-
+poppinBottles(enterInvestment: 20)
 
 
 
